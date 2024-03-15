ff041a49cd5bf1f155456301c47accd21d8b9ffe
Sam Day <me@samcday.com>
2024-03-15 10:00:12 +0100

    start somewhere

---
+++ fastboot.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cat image/usr/lib/modules/vmlinuz image/boot/dtbs/qcom/msm8916-samsung-a5u-eur.dtb > /tmp/vmlinuz-dtb
+sudo mkbootimg \
+    --kernel /tmp/vmlinuz-dtb \
+    --ramdisk image/boot/initramfs-6.6.0-msm8916.img \
+    --base "0x80000000" \
+    --second_offset "0x00f00000" \
+    --cmdline "earlycon rd.retry=30 rd.shell rd.debug log_buf_len=1M" \
+    --kernel_offset "0x00080000" \
+    --ramdisk_offset "0x02000000" \
+    --tags_offset "0x01e00000" \
+    --pagesize "2048" \
+    -o /tmp/test.img
+fastboot boot /tmp/test.img
